78b1fed1a6a1ca8752af1a96d6a2554abadf7144
avrians <avriansyahb10@gmail.com>
2023-06-24 04:41:08 +0700

    update

---
+++ src/app/Kategori.java
@@ -0,0 +1,46 @@
+package app;
+
+/**
+ *
+ * @author Avria
+ */
+public class Kategori {
+
+    private String id;
+    private String nama;
+    private String bobot;
+    private String atribut;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getBobot() {
+        return bobot;
+    }
+
+    public void setBobot(String bobot) {
+        this.bobot = bobot;
+    }
+
+    public String getAtribut() {
+        return atribut;
+    }
+
+    public void setAtribut(String atribut) {
+        this.atribut = atribut;
+    }
+
+}
